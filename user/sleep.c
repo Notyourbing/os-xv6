e9677de8972a16cb8a691a6cd04572570b647ae0
bingbing <798iu223@gmail.com>
2024-07-16 18:18:07 +0800

    lab1

---
+++ user/sleep.c
@@ -0,0 +1,24 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int 
+main(int argc,char *argv[]){
+    if(argc<=1){
+        fprintf(2,"error:Please input the ticks you want to sleep \n");
+    }
+    else if(argc==2){
+        int time=atoi(argv[1]);
+        if(time<=0){
+            fprintf(2,"error:Please input valid time\n");
+        }
+        else{
+            sleep(time);
+            exit(0);
+        }
+    }
+    else{
+        fprintf(2,"error:Please input no more than one time value\n");
+    }
+    exit(1);
+}
