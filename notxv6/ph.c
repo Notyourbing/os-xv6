ccf62161f022cf3e37e0785e7f43e83c20f445ee
bingbing <798iu223@gmail.com>
2024-07-19 12:08:07 +0800

    lab6

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -50,9 +51,12 @@ void put(int key, int value)
     // update the existing key.
     e->value = value;
   } else {
+    pthread_mutex_lock(&lock[i]);
     // the new is new.
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
+
 }
 
 static struct entry*
@@ -103,6 +107,7 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
@@ -114,7 +119,10 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  // init lock  
+  for (int i = 0; i < NBUCKET; i++){      
+    pthread_mutex_init(&lock[i], NULL);
+  } 
   //
   // first the puts
   //
